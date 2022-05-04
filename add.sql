92b6289acdf4ce74d6cc8c4a015eb052683c7429
Daniel Diaz <z1940868@hopper.cs.niu.edu>
2022-05-03 19:35:09 -0500

    updated add.sql

--- add.sql
+++ add.sql
@@ -3,6 +3,8 @@
 -- Drop Tables
 DROP TABLE Customer_Order;
 DROP TABLE Order_Product;
+DROP TABLE Order_Shipment;
+DROP TABLE Customer_Shipment;
 
 DROP TABLE Customer;
 DROP TABLE Order_;
@@ -25,7 +27,6 @@ CREATE TABLE Customer(
 
 CREATE TABLE Order_(
     Order_Num       INT             UNIQUE  PRIMARY KEY,
-    Requested_QTY   INT             NOT NULL,
     Order_Date      DATE            NOT NULL,
     Status          VARCHAR(16)     NOT NULL,
     Net_Total       INT             NOT NULL
@@ -56,5 +57,27 @@ CREATE TABLE Customer_Order(
 );
 
 CREATE TABLE Order_Product(
-    Order_Num       INT         
+    Order_Num       INT,    -- Not Unique: the same order # can have many different products
+    P_ID            INT,    -- Not Unique: The same product can be in different orders
+    Requested_QTY   INT,    -- Not Unique: The same qty can show up many different times
+                            --             in the same table 
+
+    FOREIGN KEY (Order_Num) REFERENCES Order_   (Order_Num),
+    FOREIGN KEY (P_ID)      REFERENCES Product  (P_ID)
+);
+
+CREATE TABLE Order_Shipment(
+    Order_Num   INT     UNIQUE,
+    Ship_ID     INT     UNIQUE,
+
+    FOREIGN KEY (Order_Num) REFERENCES Order_   (Order_Num),
+    FOREIGN KEY (Ship_ID)   REFERENCES Shipment (Ship_ID)
+);
+
+CREATE TABLE Customer_Shipment(
+    Email       VARCHAR(30),
+    Ship_ID     INT             UNIQUE,
+
+    FOREIGN KEY (Email)     REFERENCES  Customer(Email),
+    FOREIGN KEY (Ship_ID)   REFERENCES  Shipment(Ship_ID)
 );
