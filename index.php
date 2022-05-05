fe4732474b7b0377fadc24939953d706079e373c
Michael Marchetti <z1860574@turing.cs.niu.edu>
2022-05-05 12:38:23 -0500

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <?php
     // hide credentials
     include('login.php');
+    include('functions.php');
     // init PDO
     try { 
         $pdo = new PDO($dsn, $username, $password);
@@ -18,8 +19,9 @@
 <!-- front end display -->
 <?php
     echo "<h1> Welcome to Banana Threads Inc. </h1><br>";
+    echo "<h3> Please login: </h3>";
 
-    // Get Customer Email and Password 
+    // Get Customer Email and Password through login prompt
     echo "<form action=\"\" method=\"POST\">";
         echo "<h3> Please enter E-mail: ";
         echo "<input type=\"text\" name=\"entered_email\"/>";        
@@ -62,8 +64,39 @@
     // Go to orders
     echo "<a href='order.php'>View your Orders</a><br><br>";
 
-    // Product Select 
-    
+    echo "<h3>Product list: </h3>"; 
+    $rs = $pdo->query("SELECT P_Name, P_Price FROM Product");
+    $rows = $rs->fetchAll(PDO::FETCH_ASSOC);
+    make_table($rows);
+    ?>
 
-?>
+    <form method="post">
+        <br><label for="product">Select Product:</label>
+        <select id="product" name="product">
+            <?php
+            $rs = $pdo->query("SELECT P_Name, P_Price FROM Product;");
+            $row = $rs->fetchAll(PDO::FETCH_ASSOC);
+            echo '<option value=""></option>';
+            foreach ($row as $item) {
+                #the options to select from in the drop down
+                echo '<option value="' . $item["P_Name"] . "|" . $item["P_Price"] . '">' . $item["P_Name"] . "</option>";
+            }
+            ?>
+        </select><br>
+
+
+        <label for="quantity">Enter Quantity:</label>
+        <input type="text" id="quantity" name="quantity"><br>
+
+
+        <br><input type="submit" value="Add to Cart" />
+    </form>
+
+    <!--add to cart has been selected-->
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            echo "Item added successfully!";
+    }
+    ?>
 </body></html>
