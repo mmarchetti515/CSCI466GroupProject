dce8ab547f1ba52fef7b36be8c683c109a8db7dd
Daniel Diaz <z1940868@hopper.cs.niu.edu>
2022-05-04 23:13:51 -0500

    fixed cart.phpw

--- cart.php
+++ cart.php
@@ -3,17 +3,34 @@
 include('login.php');
 // login to MariaDB
 try { // if something goes wrong, an exception is thrown
-    $dsn = "mysql:host=courses;dbname=z1860574";
     $pdo = new PDO($dsn, $username, $password); 
     
-    #homepage
-    echo "<h1>View your cart here!</h1><br>";
-
-    echo $goToHome = "<a href='index.php'>Go to Home</a><br>";
-    echo $goToCheckout = "<a href='checkout.php'>Proceed to checkout<br></a><br>";
+    //echo $goToHome = "<a href='index.php'>Go to Home</a><br>";
+    //echo $goToCheckout = "<a href='checkout.php'>Proceed to checkout<br></a><br>";
 }
 catch(PDOexception $e) { // handle that exception
     echo "Connection to database failed: " . $e->getMessage();
 }
 ?>
+
+<?php
+    // create session for interpage communication
+    session_start();
+    $passed = $_SESSION['inter_email'];   
+
+    // debugging
+    //echo "Testing variable passing! <br>";
+    //echo "Email Passed-> " . $passed . "<br>";
+    
+    // redirection
+    // send back the email data so the user stays logged in
+    echo "<form action=\"index.php\" method=\"POST\">";
+        echo "<input type=\"hidden\" name=\"email\" value=$passed>";
+        echo "<input type=\"submit\" name=\"submit\" value=\"Go back to homepage\">";
+    echo "</form>"
+    
+    //echo "<a href=\"index.php\">Go back to homepage</a><br>";
+    //echo "<h1>View your cart here!</h1><br>";
+?> 
+
 </body></html>
